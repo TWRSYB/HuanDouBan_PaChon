58fd1fd0ff951996d21d8b6ebf1a3509a97231c6
wr <wr@qq.com>
2023-08-29 14:56:53 +0800

    优化

--- ReqUtil/ReqUtil.py
+++ ReqUtil/ReqUtil.py
@@ -11,128 +11,130 @@ from MyUtil.RcdDataJson import rcd_data_json
 
 
 class ReqUtil:
-    def __init__(self, session: Session = None, test_times: int = 5):
+
+    req_type_annotations = {1: 'get', 2: 'ajax_get', 3: 'ajax_post'}
+
+    def __init__(self, session: Session = None):
         if session:
             self.session = session
         else:
             self.session = requests.Session()
             self.session.headers = HEADERS
-            self.session.timeout = 15  # 设置超时时间为10秒
             self.session.mount('https://', requests.adapters.HTTPAdapter(pool_connections=10, pool_maxsize=60))
             self.session.mount('http://', requests.adapters.HTTPAdapter(pool_connections=10, pool_maxsize=60))
-        self.test_times = test_times
 
-    def try_get_req_times(self, url, params=None, msg="", log=com_log):
-        for i in range(self.test_times):
+    def try_get_times(self, url, timeout=30, params=None, msg="", log=com_log, test_times: int = 5):
+        """
+        发送普通get请求
+        :param url: url
+        :param timeout: 超时时间(秒), 默认30
+        :param params: 参数
+        :param msg: msg
+        :param log: log
+        :param test_times: 尝试次数, 默认5次
+        :return: Response
+        """
+        req_type = 1
+        res = self.__try_req_times(url=url, req_type=req_type, timeout=timeout, params=params, msg=msg, log=log,
+                                   test_times=test_times)
+        if res:
+            log.info(
+                f"{self.req_type_annotations[req_type]}请求成功: {msg}, url: {url}, params: {params} res: {res}")
+            return res
+
+    def try_ajax_get_times(self, url, timeout=30, params=None, msg="", log=com_log, test_times: int = 5):
+        """
+        发送Ajax get请求
+        :param url: url
+        :param timeout: 超时时间(秒), 默认30
+        :param params: 参数
+        :param msg: msg
+        :param log: log
+        :param test_times: 尝试次数, 默认5次
+        :return: Dict
+        """
+        req_type = 2
+        res = self.__try_req_times(url, req_type=req_type, timeout=timeout, params=params, msg=msg, log=log,
+                                   test_times=test_times)
+        if res:
+            dict_res = json.loads(res.text)
+            rcd_data_json.append_data_to_txt(
+                txt_file=f"{OUTPUT_DIR}/Ajax_res/ajax_get_{filename_rm_invalid_chars(url)}.txt",
+                data={'params': params, 'dict_res': dict_res}, msg=f"{self.req_type_annotations[req_type]}接口响应成功",
+                log=log)
+            log.info(
+                f"{self.req_type_annotations[req_type]}成功: {msg}, url: {url}, params: {params} res: {res}")
+            return dict_res
+
+    def try_ajax_post_times(self, url, timeout=30, params=None, data=None, msg="", log=com_log, test_times: int = 5):
+        """
+        发送Ajax post请求
+        :param url: url
+        :param timeout: 超时时间(秒), 默认30
+        :param params: 参数
+        :param data: 数据
+        :param msg: msg
+        :param log: log
+        :param test_times: 尝试次数, 默认5次
+        :return: Dict
+        """
+        req_type = 3
+        res = self.__try_req_times(url, req_type=req_type, timeout=timeout, params=params, data=data, msg=msg, log=log,
+                                   test_times=test_times)
+        if res:
+            dict_res = json.loads(res.text)
+            rcd_data_json.append_data_to_txt(
+                txt_file=f"{OUTPUT_DIR}/jsondatas/ajax_post_{filename_rm_invalid_chars(url)}.txt",
+                data={'params': params, 'data': data, 'dict_res': dict_res},
+                msg=f"{self.req_type_annotations[req_type]}接口响应成功",
+                log=log
+            )
+            log.info(
+                f"{self.req_type_annotations[req_type]}成功: {msg}, url: {url}, params: {params}, data: {data}, res: {res}")
+            return dict_res
+
+    def __try_req_times(self, url, req_type, timeout=30, params=None, data=None, msg="", log=com_log,
+                        test_times: int = 5):
+        """
+        尝试请求多次
+        :param url: url
+        :param req_type: 请求类型 {1: 'get', 2: 'ajax_get', 3: 'ajax_post'}
+        :param timeout: 超时时间(秒), 默认30
+        :param params: 参数
+        :param data: 数据
+        :param msg: msg
+        :param log: log
+        :param test_times: 尝试次数, 默认5次
+        :return: Response
+        """
+        for i in range(test_times):
             res = None
             try:
-                res = self.session.get(url=url, params=params, timeout=30)
-                if res.status_code == 200:
-                    log.info(
-                        f"get请求成功: {msg}, url: {url}, params: {params} res: {res}")
+                if req_type in [1, 2]:
+                    res = self.session.get(url=url, params=params, timeout=timeout)
+                elif req_type in [3]:
+                    res = self.session.post(url=url, params=params, data=data, timeout=timeout)
+                if res.status_code == 200 and req_type in [1]:
                     return res
-                if i < self.test_times - 1:
-                    log.warning(f"get请求响应错误: 第{i + 1}次 msg: {msg} res: {res}"
+                if res.status_code == 200 and req_type in [2, 3] and json.loads(res.text).get('code') == 200:
+                    return res
+                if i < test_times - 1:
+                    log.warning(f"{self.req_type_annotations[req_type]}请求响应错误: 第{i + 1}次 msg: {msg} res: {res}"
                                 f"\n\turl: {url}, params: {params}")
                 else:
-                    log.error(f"get请求响应错误!!! 第{i + 1}次 msg: {msg}"
-                              f"\n\turl: {url}, params: {params}")
-            except Exception as e:
-                if i < self.test_times - 1:
-                    log.warning(f"get请求出现异常: 第{i + 1}次 msg: {msg} res: {res}"
-                                f"\n\t异常: {e}"
-                                f"\n\turl: {url}, params: {params}")
-                else:
-                    log.error(f"get请求出现异常!!! 第{i + 1}次 msg: {msg} res: {res}"
-                              f"\n\t异常: {e}"
-                              f"\n\turl: {url}, params: {params}")
-
-    def try_ajax_get_times(self, url, params=None, msg="", log=com_log):
-        for i in range(self.test_times):
-            res = None
-            try:
-                res = self.session.get(url=url, params=params, timeout=30)
-                if res.status_code == 200:
-                    dict_res = json.loads(res.text)
-                    if dict_res.get('code') == 200:
-                        rcd_data_json.append_data_to_txt(
-                            txt_file=f"{OUTPUT_DIR}/jsondatas/ajax_get_{filename_rm_invalid_chars(url)}.txt",
-                            data={'params': params, 'dict_res': dict_res}, msg="ajax_get_接口响应成功", log=log)
-                        log.info(
-                            f"ajax get请求成功: {msg}, url: {url}, params: {params}"
-                            f" res: {res}")
+                    if res.status_code == 404:
+                        log.error(f"{self.req_type_annotations[req_type]}请求响应错误404!!! 第{i + 1}次 msg: {msg}"
+                                  f"\n\turl: {url}, params: {params}")
                         return res
-                    else:
-                        if i < self.test_times - 1:
-                            log.warning(
-                                f"ajax get请求响应错误: 第{i + 1}次 msg: {msg} res: {res}"
-                                f"\n\turl: {url}, params: {params}")
-                        else:
-                            log.error(f"ajax get请求响应错误!!! 第{i + 1}次 msg: {msg}"
-                                      f"\n\turl: {url}, params: {params}")
+                    log.error(f"{self.req_type_annotations[req_type]}请求响应错误!!! 第{i + 1}次 msg: {msg}"
+                              f"\n\turl: {url}, params: {params}")
             except Exception as e:
-                if i < self.test_times - 1:
-                    log.warning(f"ajax get请求出现异常: 第{i + 1}次 msg: {msg} res: {res}"
+                if i < test_times - 1:
+                    log.warning(f"{self.req_type_annotations[req_type]}请求出现异常: 第{i + 1}次 msg: {msg} res: {res}"
                                 f"\n\t异常: {e}"
                                 f"\n\turl: {url}, params: {params}")
                 else:
-                    log.error(f"ajax get请求出现异常!!! 第{i + 1}次 msg: {msg} res: {res}"
+                    log.error(f"{self.req_type_annotations[req_type]}请求出现异常!!! 第{i + 1}次 msg: {msg} res: {res}"
                               f"\n\t异常: {e}"
                               f"\n\turl: {url}, params: {params}"
                               f"{traceback.format_exc()}")
-
-    def try_ajax_post_times(self, url, params=None, data=None, msg="", log=com_log):
-        for i in range(self.test_times):
-            res = None
-            try:
-                res = self.session.post(url=url, params=params, data=data, timeout=30)
-                if res.status_code == 200:
-                    dict_res = json.loads(res.text)
-                    rcd_data_json.append_data_to_txt(
-                        txt_file=f"{OUTPUT_DIR}/jsondatas/ajax_post_{filename_rm_invalid_chars(url)}.txt",
-                        data={'params': params, 'data': data, 'dict_res': dict_res}, msg="ajax_post_接口响应成功", log=log
-                    )
-                    if dict_res.get('code') == 200:
-                        log.info(
-                            f"ajax post请求成功: {msg}, url: {url}, params: {params}, data: {data},"
-                            f" res: {res}")
-                        return res
-                    else:
-                        if i < self.test_times - 1:
-                            log.warning(f"ajax post请求成功, 但接口响应错误: 第{i + 1}次 msg: {msg}"
-                                        f"\n\t{f'dict_res: {dict_res}' if res else ''}"
-                                        f"\n\turl: {url}, params: {params}, data: {data}")
-                        else:
-                            log.error(f"ajax post请求成功, 但接口响应错误!!! 第{i + 1}次 msg: {msg}"
-                                      f"\n\t{f'dict_res: {dict_res}' if res else ''}"
-                                      f"\n\turl: {url}, params: {params}, data: {data}")
-                if i < self.test_times - 1:
-                    log.warning(f"ajax post请求响应错误: 第{i + 1}次 msg: {msg} res: {res}"
-                                f"\n\turl: {url}, params: {params}, data: {data}")
-                else:
-                    log.error(f"ajax post请求响应错误!!! 第{i + 1}次 msg: {msg}"
-                              f"\n\turl: {url}, params: {params}, data: {data}")
-            except Exception as e:
-                if i < self.test_times - 1:
-                    log.warning(f"ajax post请求出现异常: 第{i + 1}次 msg: {msg} res: {res}"
-                                f"\n\t异常: {e}"
-                                f"\n\turl: {url}, params: {params}, data: {data}")
-                else:
-                    log.error(f"ajax post请求出现异常!!! 第{i + 1}次 msg: {msg} res: {res}"
-                              f"\n\t异常: {e}"
-                              f"\n\turl: {url}, params: {params}, data: {data}")
-
-    def get_once(self, url, params=None, msg="", log=com_log):
-        res = None
-        try:
-            res = self.session.get(url=url, params=params)
-            if res.status_code == 200:
-                log.info(f"get请求成功: {msg}, url: {url}, params: {params} res: {res}")
-                return res
-            else:
-                log.error(f"get请求响应错误!!! msg: {msg} res: {res}"
-                          f"\n\turl: {url}, params: {params}")
-        except Exception as e:
-            log.error(f"get请求出现异常!!! msg: {msg}"
-                      f"\n\t异常: {e}"
-                      f"\n\turl: {url}, params: {params} res: {res}")
